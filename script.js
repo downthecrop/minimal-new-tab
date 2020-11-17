ba51772efbe6bb3b1cc54cf7fb57552521c3c2df
crop <bryce@mathx.ca>
2020-11-17 13:26:19 -0800

    basics of search

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-chrome.topSites.get(function(sites){
-  console.log(sites)
+const url = "http://suggestqueries.google.com/complete/search?client=chrome&q=";
+
+chrome.topSites.get(function (sites) {
+	console.log(sites)
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+	var lastsearch = ""
+	document.getElementById('ginput').onkeyup = function () {
+		if (document.getElementById('ginput').value != lastsearch && document.getElementById('ginput').value != "") {
+			lastsearch = document.getElementById('ginput').value
+			fetch(url + document.getElementById('ginput').value)
+				.then(res => res.json())
+				.then(data => console.log(data));
+			console.log("Last search: " + lastsearch)
+		}
+	}
 })
