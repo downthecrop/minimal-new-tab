19da335bc18aa230efb2466eefc7ac451f458d20
crop <bryce@mathx.ca>
2020-12-10 17:35:58 -0800

    cleanup

--- script.js
+++ script.js
@@ -24,12 +24,15 @@ function toDataURL(url, callback) {
     xhr.send();
 }
 
+googleDefault = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABs0lEQVR4AWL4//8/RRjO8Iucx+noO0O2qmlbUEnt5r3Juas+hsQD6KaG7dqCKPgx72Pe9GIY27btZBrbtm3btm0nO12D7tVXe63jqtqqU/iDw9K58sEruKkngH0DBljOE+T/qqx/Ln718RZOFasxyd3XRbWzlFMxRbgOTx9QWFzHtZlD+aqLb108sOAIAai6+NbHW7lUHaZkDFJt+wp1DG7R1d0b7Z88EOL08oXwjokcOvvUxYMjBFCamWP5KjKBjKOpZx2HEPj+Ieod26U+dpg6lK2CIwTQH0oECGT5eHj+IgSueJ5fPaPg6PZrz6DGHiGAISE7QPrIvIKVrSvCe2DNHSsehIDatOBna/+OEOgTQE6WAy1AAFiVcf6PhgCGxEvlA9QngLlAQCkLsNWhBZIDz/zg4ggmjHfYxoPGEMPZECW+zjwmFk6Ih194y7VHYGOPvEYlTAJlQwI4MEhgTOzZGiNalRpGgsOYFw5lEfTKybgfBtmuTNdI3MrOTAQmYf/DNcAwDeycVjROgZFt18gMso6V5Z8JpcEk2LPKpOAH0/4bKMCAYnuqm7cHOGHJTBRhAEJN9d/t5zCxAAAAAElFTkSuQmCC"
 
 function getFavicons(sites, i) {
     if (localStorage.getItem(i) === null) {
         toDataURL(iconurl + sites[i - 1].url, function (dataUrl) {
             v = dataUrl;
-            localStorage.setItem(i - 1, v)
+            if (v != googleDefault){
+                localStorage.setItem(i - 1, v)
+            }
         })
         if (i <= 8) {
             Promise.resolve(getFavicons(sites, i + 1))
@@ -132,25 +135,16 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    document.getElementById("ext-menu").addEventListener("click", function () {
-        //localStorage.clear()
-        //window.location.reload()
-    });
-
     document.getElementById("clear-storage").addEventListener("click", function () {
         localStorage.clear()
         window.location.reload()
     });
-    // Get the modal
+
+    //settings modal
     var modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
     var btn = document.getElementById("settings-menu");
-
-    // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on the button, open the modal
     btn.onclick = function () {
         modal.style.display = "block";
     }
@@ -160,7 +154,6 @@ document.addEventListener('DOMContentLoaded', function () {
         modal.style.display = "none";
     }
 
-    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
